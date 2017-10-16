57f5f7b39ef17c1112ed91bbcf8c7eb929efaa6d
Mohit Arun Inampudi <mi9.developer@gmail.com>
2017-10-15 20:04:24 -0400

    added authorization

---
+++ src/main/java/com/wsproducer/handler/ProducerPasswordCallback.java
@@ -0,0 +1,31 @@
+package com.wsproducer.handler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class ProducerPasswordCallback implements CallbackHandler {
+
+	Map<String, String> credentials = new HashMap<>();
+
+	public ProducerPasswordCallback() {
+		credentials.put("admin", "password");
+	}
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		for (Callback callback : callbacks) {
+			WSPasswordCallback wspc = (WSPasswordCallback) callback;
+			String string = credentials.get(wspc.getIdentifier());
+			wspc.setPassword(string);
+			return;
+		}
+	}
+
+}
